6169a75280cc8b0e8d4ad4325c911b9e8cd6f085
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-04-09 21:53:05 +0900

    feat: add custom error handling for AppError instances in error middleware

--- src/middleware/error-handler.middleware.ts
+++ src/middleware/error-handler.middleware.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from "express";
 import { HTTP_STATUS } from "../config/http.config";
+import { AppError } from "../utils/app-error";
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   console.log(`Error occured: ${req.path}`, error);
@@ -7,6 +8,13 @@ export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   if (error instanceof SyntaxError) {
     return res.status(HTTP_STATUS.BAD_REQUEST).json({
       message: "Invalid JSON format.",
+    });
+  }
+
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+      errorCode: error.errorCode,
     });
   }
 
