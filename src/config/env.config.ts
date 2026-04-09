b7d5dc5ae6543190b80c4ce5ebfc9b101db4a7c6
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-04-09 21:25:43 +0900

    feat: add HTTP status constants, configure CORS, and implement server startup with environment variables

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,5 +4,6 @@ export const env = {
   NODE_ENV: getEnv("NODE_ENV", "development"),
   PORT: getEnv("PORT", "3001"),
   BASE_URL: getEnv("BASE_URL"),
+  MONGO_URI: getEnv("MONGO_URI"),
   FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "http://localhost:5173"),
 };
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { HTTP_STATUS } from "./config/http.config";
+import { env } from "./config/env.config";
 
 const app = express();
 
@@ -9,9 +11,21 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  cors({
+    origin: [env.FRONTEND_ORIGIN],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.get("/health", (req: Request, res: Response) => {
-  res.status(200).json({
+  res.status(HTTP_STATUS.OK).json({
     message: "Server is health",
     status: "Ok",
   });
+});
+
+app.listen(env.PORT, async () => {
+  console.log(`Server running on port ${env.PORT} in ${env.NODE_ENV} mode`);
 });
